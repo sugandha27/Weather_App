c29159e201f176f8d0111b10f871fec6fd068d43
Sugandha27 <Sugandhabhandari2011@gmail.com>
2021-02-04 23:17:22 +0530

    search widget

--- lib/changeCity.dart
+++ lib/changeCity.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class ChangeCity extends StatelessWidget {
+  var _cityFieldController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -11,14 +12,55 @@ class ChangeCity extends StatelessWidget {
         title: Text(
           'Search City',
           style: TextStyle(
-            // color: HexColor('#F8F8F8'),
             color: HexColor('#F6F7F3'),
             fontWeight: FontWeight.w700,
           ),
         ),
         centerTitle: false,
       ),
-      body: new Stack(),
+      body: new ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: new ListTile(
+              tileColor: HexColor('#F6F7F3'),
+              title: new TextField(
+                keyboardType: TextInputType.text,
+                controller: _cityFieldController,
+                decoration: new InputDecoration(
+                  hintText: "Enter City",
+                  isDense: true,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0, 30, 0),
+            child: new ListTile(
+              title: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(
+                        context, {'enter': _cityFieldController.text});
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: new Text(
+                      "Get Weather",
+                      style: TextStyle(
+                        color: HexColor('#F6F7F3'),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  color: HexColor('#0F0D35'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
